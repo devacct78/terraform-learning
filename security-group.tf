d7ac8789247d99eded4879a8767cf5e8e121c186
Sai <devacct78@gmail.com>
2025-03-22 23:36:59 +0530

    Provision SG For webserver

--- security-group.tf
+++ security-group.tf
@@ -24,3 +24,38 @@ resource "aws_security_group" "vpc_ssh" {
       Name = "vpc-ssh"
     }
 }
+
+#SG for webserver
+resource "aws_security_group" "vpc_web" {
+    name = "vpc-web"
+    description = "SG for web"
+    vpc_id = "vpc-0d5c9732d76cda92f" #if vpc_id not configured, tf will use the default VPC.
+
+    ingress {
+        description = "Allow port 80 for HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "Allow port 443 for HTTPS"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        description = "Allow all outgoing traffic"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+      Name = "vpc-web"
+    }
+}
