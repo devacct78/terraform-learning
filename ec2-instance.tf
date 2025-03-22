50403d864c0ec17e3ee6252c2c47b9e56f592027
Sai <devacct78@gmail.com>
2025-03-22 22:42:33 +0530

    make ec2 tf use instance type variable

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my_ec2" {
   ami           = "ami-08b5b3a93ed654d19"
-  instance_type = "t2.micro"
-  user_data = file("${path.module}/nginx.sh")
+  instance_type = var.ec2_instance_type
+  user_data     = file("${path.module}/nginx.sh")
   tags = {
     "Name" = "nginx-ec2"
   }
