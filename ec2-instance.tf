3a3bfd608ec2f2024dda607f1e56762bf976c0b3
Sai <devacct78@gmail.com>
2025-03-22 22:30:36 +0530

    execute user data through tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,4 +1,8 @@
 resource "aws_instance" "my_ec2" {
   ami           = "ami-08b5b3a93ed654d19"
   instance_type = "t2.micro"
+  user_data = file("${path.module}/nginx.sh")
+  tags = {
+    "Name" = "nginx-ec2"
+  }
 }
