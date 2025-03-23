5cafc0e4dac4dc837f0bacf6ce7409ff15eaaed1
Sai <devacct78@gmail.com>
2025-03-23 10:47:52 +0530

    terraform fmt

--- c2-variables.tf
+++ c2-variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
-    description = "Region in which the AWS Resources will be provisioned"
-    type = string
-    default = "us-east-1"
+  description = "Region in which the AWS Resources will be provisioned"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "ec2_instance_type" {
-    description = "Configuration with which the ec2 will launch"
-    type = string
-    default = "t2.micro"
+  description = "Configuration with which the ec2 will launch"
+  type        = string
+  default     = "t2.micro"
 }
