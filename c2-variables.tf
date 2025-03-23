3b6f5962fa3afaeda29f96808c000d013e670474
Sai <devacct78@gmail.com>
2025-03-23 11:16:17 +0530

    remove key pair hardcoding and reference via input variable

--- c2-variables.tf
+++ c2-variables.tf
@@ -9,3 +9,9 @@ variable "ec2_instance_type" {
   type        = string
   default     = "t2.micro"
 }
+
+variable "key_pair" {
+  description = ".pem Key which will be used to connect to EC2 remotely using SSH"
+  type        = string
+  default     = "terraform-key"
+}
--- ec2-instance.tf
+++ ec2-instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "my_ec2" {
   ami                    = data.aws_ami.ami_name.id
   instance_type          = var.ec2_instance_type
   user_data              = file("${path.module}/nginx.sh")
-  key_name               = "terraform-key"
+  key_name               = var.key_pair
   vpc_security_group_ids = [aws_security_group.vpc_ssh.id, aws_security_group.vpc_web.id]
   tags = {
     "Name" = "nginx-ec2"
