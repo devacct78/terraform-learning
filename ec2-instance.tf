d64e32586cd76c3c61d753b65c6b5b4c4823194d
Sai <devacct78@gmail.com>
2025-03-23 11:00:36 +0530

    add SG's to ec2, and add keypair for SSH

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "my_ec2" {
-  ami           = data.aws_ami.ami_name.id
-  instance_type = var.ec2_instance_type
-  user_data     = file("${path.module}/nginx.sh")
+  ami                    = data.aws_ami.ami_name.id
+  instance_type          = var.ec2_instance_type
+  user_data              = file("${path.module}/nginx.sh")
+  key_name               = "terraform-key"
+  vpc_security_group_ids = [aws_security_group.vpc_ssh.id, aws_security_group.vpc_web.id]
   tags = {
     "Name" = "nginx-ec2"
   }
