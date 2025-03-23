6b17b14b768d70a879d3fd7dae9739830d6db799
Sai <devacct78@gmail.com>
2025-03-23 10:31:50 +0530

    adding data source for aws_ami and make ec2 tf read it

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-08b5b3a93ed654d19"
+  ami           = data.aws_ami.ami_name.id
   instance_type = var.ec2_instance_type
   user_data     = file("${path.module}/nginx.sh")
   tags = {
